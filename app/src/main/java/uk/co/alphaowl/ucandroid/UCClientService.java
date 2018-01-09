dd00659dd7eb4a805a950580877d68005f93fb90
Wing Chau <wing@devtography.com>
2018-01-09 12:23:25 +0000

    Created UCClientService
    Returns the current service instance on bound to an activity so clients can access the public methods in the service.

---
+++ app/src/main/java/uk/co/alphaowl/ucandroid/UCClientService.java
@@ -0,0 +1,28 @@
+package uk.co.alphaowl.ucandroid;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class UCClientService extends Service {
+    // Binder given to clients
+    private final IBinder mBinder = new UCClientServiceBinder();
+
+    public class UCClientServiceBinder extends Binder {
+        UCClientService getService() {
+            // Return this instance of UCClientService so clients can call public methods
+            return UCClientService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return mBinder;
+    }
+
+    /* method for clients */
+
+}
