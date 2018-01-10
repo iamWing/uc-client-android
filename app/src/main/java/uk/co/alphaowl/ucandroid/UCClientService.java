b5f99d6e8b731e013472be53f94566c4f6326be1
Wing Chau <wing@devtography.com>
2018-01-10 01:20:34 +0000

    Refactored listener interface

--- app/src/main/java/uk/co/alphaowl/ucandroid/UCClientService.java
+++ app/src/main/java/uk/co/alphaowl/ucandroid/UCClientService.java
@@ -1,10 +1,14 @@
 package uk.co.alphaowl.ucandroid;
 
-
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+
+import java.io.IOException;
+
+import uk.co.alphaowl.uc.IUCCallback;
+import uk.co.alphaowl.uc.UCClient;
 
 public class UCClientService extends Service {
     // Binder given to clients
@@ -25,17 +29,65 @@ public class UCClientService extends Service {
 
     /* interfaces */
 
-    public interface IServiceListener {
+    public interface IUCServiceListener {
 
     }
 
     /* method for clients */
 
-    private IServiceListener listener;
+    private IUCServiceListener mListener;
 
-    public void setServiceListener(IServiceListener listener) {
-        this.listener = listener;
+    private Thread mWorker;
+    private UCConnectionRunnable mRunner;
+
+    public void setServiceListener(IUCServiceListener listener) {
+        mListener = listener;
     }
 
+    public void init(String ip, int port, int bufferSize, IUCCallback callback) {
+        if (bufferSize < -1 || bufferSize == 0)
+            throw new IllegalArgumentException(
+                    "Invalid buffer size. Buffer size must not be < -1 or equals 0."
+            );
+
+        if (mRunner == null) {
+            mRunner = new UCConnectionRunnable(ip, port, bufferSize, mListener, callback);
+            mWorker = new Thread(mRunner);
+            mWorker.start();
+        }
+    }
+
+    /* for run in background */
+
+    class UCConnectionRunnable implements Runnable {
+
+        private IUCServiceListener mListener;
+        private IUCCallback mCallback;
+
+        private UCClient instance;
+
+        private String mIp;
+        private int mPort;
+        private int mBufferSize;
+
+        UCConnectionRunnable(String ip, int port, int bufferSize,
+                             IUCServiceListener listener, IUCCallback callback) {
+            mIp = ip;
+            mPort = port;
+            mBufferSize = bufferSize;
+            mListener = listener;
+            mCallback = callback;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                instance = UCClient.init(mIp, mPort, mBufferSize, mCallback);
+            } catch (IOException ex) {
+
+            }
+        }
+    }
 
 }
