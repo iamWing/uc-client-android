eee521095ef22f9e5f9aac697a25a70cdef5ec17
Wing Chau <wing@devtography.com>
2018-01-10 09:34:39 +0000

    Created DpadActivity

---
+++ app/src/main/java/uk/co/alphaowl/uc/sample/DpadActivity.java
@@ -0,0 +1,13 @@
+package uk.co.alphaowl.uc.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DpadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dpad);
+    }
+}
